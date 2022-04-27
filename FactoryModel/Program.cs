15c3aea53fd211d38bcf1a6fef892adebd830289
benbenzlj <76167233@qq.com>
2022-04-27 10:21:18 +0800

    add factoryModel Code

--- FactoryModel/Program.cs
+++ FactoryModel/Program.cs
@@ -1,2 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace DesignPatterns.FactoryMethod.Conceptual
+{
+    abstract class Creator
+    {
+        public abstract IProduct FactoryMethod();
+
+    }
+
+    class ConcreteCreator1 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct1();
+        }
+    }
+
+    class ConcreteCreator2 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct2();
+        }
+    }
+
+    public interface IProduct
+    {
+        string Operation();
+    }
+
+    class ConcreteProduct1 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Result of ConcreteProduct1";
+        }
+    }
+
+    class ConcreteProduct2 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Result  of ConcreteProduct2}";
+        }
+    }
+
+    class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("App: Launched with the ConcreteCreator1.");
+
+        }
+    }
+
+}
